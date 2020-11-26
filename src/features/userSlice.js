63575e204bf9ca7a25e3273c4ed4570116e3c47b
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2020-11-26 18:16:32 +0600

    add package

---
+++ src/features/userSlice.js
@@ -0,0 +1,36 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+    name:"user",
+    initialState:{
+        isSignedIn:false,
+        userData:null,
+        searchInput:"tech",
+        blogData: null,
+    },
+    reducers:{
+        setSignedIn:(state,action)=>{
+            state.isSignedIn=action.payload;
+        },
+        setUserData:(state,action)=>{
+            state.userData=action.payload;
+        },
+        setInput:(state,action)=>{
+            state.searchInput= action.payload;
+        },
+        setBlogData:(state,action)=>{
+            state.blogData=action.payload;
+        },
+    },
+});
+
+export const 
+{setSignedIn, 
+    setUserData,
+    setInput,
+    setBlogData,
+}=userSlice.action;
+export const selectSignedIn=(state)=> state.user.isSignedIn;
+export const selectUserData=(state)=> state.user.userData;
+export const selectSignedIn=(state)=> state.user.isSignedIn;
+export const selectSignedIn=(state)=> state.user.isSignedIn;
